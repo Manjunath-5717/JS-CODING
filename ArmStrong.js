717ab9952e453107c55a9608601b2297cd625356
Manjunath <manjunathragati704@gmail.com>
2026-09-21 16:04:12 +0530

    Update ArmStrong.js

--- ArmStrong.js
+++ ArmStrong.js
@@ -11,3 +11,17 @@ function isArmStrong(n){
   return ori===sum;
 }
 console.log(isArmStrong(153));
+
+function isAramStrong(num){
+  let original=num;
+  let sum=0;
+  let digits=String(num).length;
+
+while(num!=0){
+  let digit=num%10;
+  sum=sum+Math.pow(digit,digits);
+  n=Math.floor(num/10);
+}
+  return original===sum;
+}
+console.log(isAramStrong(153));
